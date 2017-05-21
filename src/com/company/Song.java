c7ddb3214bd5faae42cda7385330f632c2d975c9
Jarad Touchberry <you@example.com>
2017-05-21 14:45:02 -0400

    working code

---
+++ src/com/company/Song.java
@@ -0,0 +1,23 @@
+package com.company;
+
+/**
+ * Created by jaradtouchberry on 5/21/17.
+ */
+public class Song {
+    private String title;
+    private double duration;
+
+    public Song(String title, double duration) {
+        this.title = title;
+        this.duration = duration;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    @Override
+    public String toString() {
+        return this.title + ": " + this.duration;
+    }
+}
